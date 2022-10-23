beba7800b9281fc1f8aa8d5e9946b448e3314e70
João GabCM(2019-PO) <38107176+Numbess@users.noreply.github.com>
2022-10-22 22:52:47 -0300

    Atualizando Socket_Servidor.py
    Adicionando bibliotecas a serem utilizadas no código além das que foram importadas incialmente junto do método "resource_path" para adição de imagens a interface gráfica do código também.

--- Socket_Servidor.py
+++ Socket_Servidor.py
@@ -6,8 +6,35 @@
 *
 """
 
+#Bibliotecas para manipulação dos dados via Socket comm o uso de Threads:
+import socket, threading
+
+
+#Bibliotecas para manipulação dos arquivos de imagem utilizados no código para poder compactá-los no arquivo .exe quando for construido:
+import sys
+import os
+
+
+#Bibliotecas para manipulação e construção da interface gráfica no Tkinter:
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+import tkinter.font as font
+
 from AnimatedGIF import *
+
+#
+#   Abaixo o método que utilizo para armazenar todos os caminhos de imagens usadas aqui nesse código para poder transformá-lo em um .exe.
+#
+#   *** Caso esteja interessado, eu segui o procedimento do seguinte link do Stackoverflow para converter meus códigos em um .exe todo o
+#       crédito aos que falam desse método nas respostas do Stackoverflow: https://stackoverflow.com/questions/54210392/how-can-i-convert-pygame-to-exe
+#
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 """
 *
